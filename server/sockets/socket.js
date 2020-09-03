9d855898c7277c85b74c1ea49f078a3ddd6e7034
Carlos Medina <carlosgerman@gmail.com>
2020-09-03 15:29:05 -0400

    Adding ui to the chat

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -22,6 +22,7 @@ io.on('connection', (client) => {
         usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
         client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió al chat`));
 
         callback(usuarios.getPersonasPorSala(data.sala));
     });
@@ -36,11 +37,12 @@ io.on('connection', (client) => {
         client.broadcast.emit
     });
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
 
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+        callback(mensaje);
     });
 
     client.on('mensajePrivado', (data) => {
